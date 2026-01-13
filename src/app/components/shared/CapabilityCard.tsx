2303838a8c77b5104144b92c4f395ba0580dad08
Maxim Isachenko <maksim.isachanka@f-suite.com>
2026-01-13 15:33:58 +0300

    Adaptive for GuaranteesSection + fix adaptive for other sections

--- src/app/components/shared/CapabilityCard.tsx
+++ src/app/components/shared/CapabilityCard.tsx
@@ -4,7 +4,7 @@ interface CapabilityCardProps {
 
 export function CapabilityCard({ text }: CapabilityCardProps) {
   return (
-    <div className="w-full sm:w-[calc(50%-10px)] lg:basis-0 content-stretch flex flex-col gap-2 sm:gap-2.5 md:gap-[10px] lg:grow items-start min-h-px min-w-px relative">
+    <div className="w-[calc(50%-8px)] sm:w-[calc(50%-10px)] lg:basis-0 content-stretch flex flex-col gap-2 sm:gap-2.5 md:gap-[10px] lg:grow items-start min-h-px min-w-px relative">
       <div className="bg-[#d9d9d9] shrink-0 size-10 sm:size-11 md:size-[46px]" />
       <p className="font-normal leading-[120%] md:leading-[100%] tracking-[0%] min-w-full not-italic relative text-xs sm:text-sm md:text-[14px] text-black w-full">
         {text}
